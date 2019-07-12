7f8a8092dc38882cd21a8d612fe2ce2241515191
CodeMonk263 <shaurya.vijayvargiya@gmail.com>
2019-07-12 13:45:42 +0530

    Added SWD, Campus Info, Campus Map

---
+++ app/src/main/java/com/crux/bphcfreshers/CampusMapFragment.java
@@ -0,0 +1,36 @@
+package com.crux.bphcfreshers;
+
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class CampusMapFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        return inflater.inflate(R.layout.fragment_campusmap, null);
+
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        view.findViewById(R.id.fg1_button1).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getContext(), "Inside Campus Map Fragment", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+}
---
+++ app/src/main/java/com/crux/bphcfreshers/MainActivity.java
@@ -0,0 +1,158 @@
+package com.crux.bphcfreshers;
+
+import android.content.ComponentName;
+import android.graphics.Color;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.customtabs.CustomTabsClient;
+import android.support.customtabs.CustomTabsIntent;
+import android.support.customtabs.CustomTabsServiceConnection;
+import android.support.customtabs.CustomTabsSession;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.view.View;
+import android.support.v4.view.GravityCompat;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.view.MenuItem;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+
+    public void TimeTable () {
+
+        final String url = "http://swd.bits-hyderabad.ac.in/";
+        CustomTabsIntent customTabsIntent;
+        final CustomTabsClient[] customTabsClient = new CustomTabsClient[1];
+        final CustomTabsSession[] customTabsSession = new CustomTabsSession[1];
+
+        CustomTabsServiceConnection customTabsServiceConnection = new CustomTabsServiceConnection() {
+            @Override
+            public void onCustomTabsServiceConnected(ComponentName componentName, CustomTabsClient client) {
+                customTabsClient[0] = client;
+                customTabsClient[0].warmup(0L);
+                customTabsSession[0] = customTabsClient[0].newSession(null);
+
+
+            }
+
+            @Override
+            public void onServiceDisconnected(ComponentName name) {
+                customTabsClient[0] = null;
+
+            }
+        };
+
+
+        CustomTabsClient.bindCustomTabsService(this, String.valueOf(R.layout.activity_main), customTabsServiceConnection);
+        customTabsIntent = new CustomTabsIntent.Builder(customTabsSession[0])
+                .setShowTitle(true)
+                .setToolbarColor(Color.rgb(80,70,250))
+                .build();
+
+        customTabsIntent.launchUrl(this, Uri.parse(url));
+
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+        navigationView.setNavigationItemSelectedListener(this);
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.screenMain, new CInfoFragment());
+        fragmentTransaction.commit();
+
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+
+        Fragment fragment = null;
+
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.cinfo) {
+            fragment = new CInfoFragment();
+        }else if (id == R.id.campusMap) {
+            fragment = new CampusMapFragment();
+        } else if (id == R.id.messInfo) {
+
+        } else if (id == R.id.libInfo) {
+
+        } else if (id == R.id.accInfo) {
+            TimeTable();
+        } else if (id == R.id.timeTable) {
+            fragment = new TimeTableFragment();
+        } else if (id == R.id.nav_share) {
+
+        } else if (id == R.id.nav_send) {
+
+        }
+
+        if (fragment != null) {
+            FragmentManager fragmentManager = getSupportFragmentManager();
+            FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+            fragmentTransaction.replace(R.id.screenMain, fragment);
+            fragmentTransaction.commit();
+        }
+
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+}
