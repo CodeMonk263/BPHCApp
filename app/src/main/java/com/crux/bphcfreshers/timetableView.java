d6bfeb1749c0dede7e94b9b59b35899afaa66286
CodeMonk263 <shaurya.vijayvargiya@gmail.com>
2019-07-14 13:46:53 +0530

    Added Buttons which show class and course upon entering

--- app/src/main/java/com/crux/bphcfreshers/timetableView.java
+++ app/src/main/java/com/crux/bphcfreshers/timetableView.java
@@ -1,20 +1,14 @@
 package com.crux.bphcfreshers;
 
-import android.app.Activity;
 import android.content.DialogInterface;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.text.InputType;
 import android.view.View;
 import android.widget.Button;
-import android.os.SystemClock;
-import android.view.View;
-import android.view.View.OnClickListener;
 import android.widget.EditText;
-import android.widget.Toast;
-
-import org.w3c.dom.Text;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -97,14 +91,77 @@ public class timetableView extends AppCompatActivity {
 
     }
 
+    public void createAlert(final Button tempButton) {
+
+        final EditText[] input = new EditText[2];
+
+        final TextView[] textfields = new TextView[2];
+
+        final String[] input_Text = new String[2];
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(timetableView.this);
+
+        LinearLayout linearLayout = new LinearLayout(timetableView.this);
+        linearLayout.setOrientation(LinearLayout.VERTICAL);
+
+        LinearLayout sublinearLayout1 = new LinearLayout(timetableView.this);
+        sublinearLayout1.setOrientation(LinearLayout.HORIZONTAL);
+        textfields[0] = new TextView((getApplicationContext()));
+        textfields[0].setText("Course:");
+        textfields[0].setTextSize(20);
+        textfields[0].setPadding(10,10,20,10);
+        input[0] = new EditText(getApplicationContext());
+        input[0].setWidth(500);
+        sublinearLayout1.addView(textfields[0]);
+        sublinearLayout1.addView(input[0]);
+
+        LinearLayout sublinearLayout2 = new LinearLayout(timetableView.this);
+        sublinearLayout2.setOrientation(LinearLayout.HORIZONTAL);
+        textfields[1] = new TextView(getApplicationContext());
+        textfields[1].setText("Class:");
+        textfields[1].setTextSize(20);
+        textfields[1].setPadding(10,10,20,10);
+        input[1] = new EditText(getApplicationContext());
+        input[1].setWidth(500);
+        sublinearLayout2.addView(textfields[1]);
+        sublinearLayout2.addView(input[1]);
+
+        linearLayout.addView(sublinearLayout1);
+        linearLayout.addView(sublinearLayout2);
+        builder.setView(linearLayout);
+
+        builder.setTitle("Enter Data");
+        builder.setIcon(R.drawable.ic_info_outline_black_24dp);
+        builder.setMessage("Please Fill Course Name, Class No.");
+
+        builder.setPositiveButton("ENTER", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                input_Text[0] = input[0].getText().toString();
+                input_Text[1] = input[1].getText().toString();
+                String finalText = input_Text[0] + "\n" +input_Text[1];
+                tempButton.setText(finalText);
+                tempButton.setTextSize(12);
+                tempButton.animate().alpha(1).setDuration(100);
+
+            }
+        });
+
+        builder.setNegativeButton("CANCEL", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+
+        builder.create().show();
+
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_timetable_view);
-
-        final EditText[] input = new EditText[1];
-
-        final String[] input_Text = new String[1];
 
         createButtonsArray();
 
@@ -118,33 +175,7 @@ public class timetableView extends AppCompatActivity {
                     @Override
                     public void onClick(View v) {
 
-                        AlertDialog.Builder builder = new AlertDialog.Builder(timetableView.this);
-                        builder.setTitle("Enter Data");
-                        builder.setIcon(R.drawable.ic_info_outline_black_24dp);
-                        builder.setMessage("PLease Fill Class Info");
-
-                        input[0] = new EditText(getApplicationContext());
-                        builder.setView(input[0]);
-
-                        builder.setPositiveButton("ENTER", new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialog, int which) {
-                                input_Text[0] = input[0].getText().toString();
-                                tempButton.animate().alpha(0).setDuration(100);
-
-                            }
-                        });
-
-                        builder.setNegativeButton("CANCEL", new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialog, int which) {
-                                dialog.dismiss();
-                            }
-                        });
-
-                        builder.create().show();
-
-                        tempButton.animate().alpha(1f).setDuration(150);
+                        createAlert(tempButton);
 
                     }
                 });
@@ -153,10 +184,3 @@ public class timetableView extends AppCompatActivity {
 
     }
 }
-
-
-
-/**
-
-
- */
