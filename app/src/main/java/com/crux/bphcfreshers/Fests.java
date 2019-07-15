897848193515bcae5b02a81669f1f87a87f34f9e
thealchemist1307 <46978534+thealchemist1307@users.noreply.github.com>
2019-07-16 04:08:47 +0530

    MAP changed

--- app/src/main/java/com/crux/bphcfreshers/Fests.java
+++ app/src/main/java/com/crux/bphcfreshers/Fests.java
@@ -8,6 +8,6 @@ public class Fests extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_nearby);
+        setContentView(R.layout.activity_fests);
     }
 }
