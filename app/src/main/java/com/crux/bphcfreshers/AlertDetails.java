2a513e4077d0e10afc5e8222052a430ef8232860
thealchemist1307 <46978534+thealchemist1307@users.noreply.github.com>
2019-07-28 13:14:53 +0530

    update

--- app/src/main/java/com/crux/bphcfreshers/AlertDetails.java
+++ app/src/main/java/com/crux/bphcfreshers/AlertDetails.java
@@ -4,8 +4,10 @@ import android.app.Application;
 import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Context;
 import android.content.ContextWrapper;
+import android.content.Intent;
 import android.os.Build;
 
 import androidx.core.app.NotificationCompat;
@@ -54,6 +56,8 @@ public class AlertDetails extends ContextWrapper {
     }
 
     public NotificationCompat.Builder getChannel() {
+        Intent intent=new Intent(AlertDetails.this,timetableView.class);
+        PendingIntent pendingIntent=PendingIntent.getActivity(this,12,intent,PendingIntent.FLAG_UPDATE_CURRENT);
         return new NotificationCompat.Builder(getApplicationContext(),CHANNEL_ID)
                 .setSmallIcon(R.drawable.ic_alarm_black_24dp)
                 .setContentTitle("Get Up!")
@@ -61,6 +65,7 @@ public class AlertDetails extends ContextWrapper {
                 .setContentInfo("You have class")
                 .setPriority(NotificationCompat.PRIORITY_MAX)
                 .setVisibility(NotificationCompat.VISIBILITY_PUBLIC)
-                .setAutoCancel(true);
+                .setAutoCancel(true)
+                .setContentIntent(pendingIntent);
     }
 }
